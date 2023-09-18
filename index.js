aa565ae552cb5fbde89c3de0a5160d3c1d628161
+ <+>
2023-09-18 12:46:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser="Angela";
+let welcomeMessage=`Welcome to Flatbook, ${currentUser} !`;
+let  excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting=`Welcome, ${firstInitial}!`
